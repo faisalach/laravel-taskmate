0df1015dda431cdb098b6e1fa916218f7ae7eef4
faisalach <faisalachramsyah@gmail.com>
2023-12-11 19:04:54 +0700

    authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+
+            $user   = Auth::user();
+			$token  = $user->createToken("token")->plainTextToken;
+ 
+            return response()->json([
+                "status"    => "success",
+                "message"   => "Successfuly Login",
+                "token"     => $token
+            ]);
+        }
+ 
+        return response()->json([
+            "status"    => "error",
+            "message"   => "Incorrect email / password",
+        ],422);
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            "status"    => "success",
+            "message"   => "Successfuly logout",
+        ]);
+    }
+
 }
